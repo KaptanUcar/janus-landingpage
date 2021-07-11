39ac48c62b66d5f4db30dad0a0e7ca6f8922e3f4
KaptanUcar <hutku2020@gmail.com>
2021-07-11 05:05:23 +0300

    First release

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (() => {
+
+eval("$('.screenshot-gallery').flickity({\n  imagesLoaded: true,\n  percentPosition: true,\n  wrapAround: true,\n  prevNextButtons: false,\n  autoPlay: 3000\n  /*cellAlign: 'left',\r\n  contain: true,\r\n  prevNextButtons: false,*/\n\n  /*wrapAround: true,*/\n\n  /*autoPlay: 3000,*/\n\n  /*fullscreen: true,\r\n  lazyLoad: 1,*/\n\n});\n\n//# sourceURL=webpack://janus-landingpage/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$('.screenshot-gallery').flickity({
+    imagesLoaded: true,
+    percentPosition: true,
+    wrapAround: true,
+    prevNextButtons: false,
+    autoPlay: 3000,
+    /*cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,*/
+    /*wrapAround: true,*/
+    /*autoPlay: 3000,*/
+    /*fullscreen: true,
+    lazyLoad: 1,*/
+})
